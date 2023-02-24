3a940f3e32e110df1cbf09fd9ff71ba16751fb3f
gmerola03 <merolag03@gmail.com>
2023-02-24 11:42:08 +0100

    Cosro react 24/02/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import './styles/App.scss';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Contatti from './pages/Contatti';
+import Blog from './pages/Blog';
+import ChiSono from './pages/ChiSono';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-
-    <div className='App'>
-      <Navbar />  
+    <Router>
+      
+      <div className='App'>
+      <Navbar /> 
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/chi-sono' element={<ChiSono/>}/>
+        <Route exact path='/blog' element={<Blog/>}/>
+        <Route exact path='/contatti' element={<Contatti/>}/>
+      </Routes> 
       <Footer />
-    </div>
+      </div>
+
+    </Router>
+    
     
   );
 }
