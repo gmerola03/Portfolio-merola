999835a595b2b8b41c2cd5750cc9c72f472e1ac9
gmerola03 <merolag03@gmail.com>
2023-02-24 11:28:24 +0100

    robe

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './styles/App.scss';
+
+const App = () => {
+  return (
+
+    <div className='App'>
+      <Navbar />  
+      <Footer />
+    </div>
+    
+  );
+}
+
+
+
+export default App;
